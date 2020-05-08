5d87b1c99f576a2db4f4544a04f8bcb3f0b27227
gbrltv <gabrielmrqstvrs@gmail.com>
2020-05-08 06:55:24 -0300

    Remove artifacts test

--- tests/test_perspective_correction.py
+++ tests/test_perspective_correction.py
@@ -1,6 +1,6 @@
 from covidxpert import load_image, perspective_correction
 from covidxpert.perspective_correction.get_corners import get_corners
-from covidxpert.perspective_correction.add_padding import add_padding
+from covidxpert.utils.padding import add_padding
 from covidxpert.perspective_correction.get_masked_image import get_masked_image
 from covidxpert.perspective_correction.cut_bounding_box import cut_bounding_box
 from covidxpert.perspective_correction.perspective_correction import get_new_cardinals
@@ -17,7 +17,7 @@ def test_perspective_correction():
         cut_image = perspective_correction(original)
         fig, axes = plt.subplots(nrows=2, ncols=2)
         axes = axes.ravel()
-        
+
         axes[0].imshow(original, cmap="gray")
         axes[0].set_title("Original image")
         padded = add_padding(original)
@@ -36,7 +36,7 @@ def test_perspective_correction():
             axes[2].scatter(*corners.T, marker='.')
             axes[2].scatter(*new_corners.T, marker='.')
             axes[2].set_title("Repositioned corners")
-            
+
         axes[3].imshow(cut_image, cmap="gray")
         axes[3].set_title("Perspective correction")
         [ax.set_axis_off() for ax in axes.ravel()]
