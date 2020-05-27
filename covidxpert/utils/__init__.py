c776c66d09cecbeb83137141d16c4c55089b0ede
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2020-05-27 09:40:48 +0200

    Added to the inport darken

--- covidxpert/utils/__init__.py
+++ covidxpert/utils/__init__.py
@@ -3,6 +3,7 @@ from .normalize_image import normalize_image
 from .remove_artefacts import remove_artefacts
 from .padding import trim_padding, add_padding
 from .inpaint import inpaint
+from .darken import darken
 
 __all__ = [
     "load_image",
@@ -10,5 +11,6 @@ __all__ = [
     "remove_artefacts",
     "trim_padding",
     "add_padding",
-    "inpaint"
+    "inpaint",
+    "darken"
 ]
