2990b0cabfb282884d7d68c6abb86d29901b1aee
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2020-05-29 16:13:47 +0200

    Added detect_almost_vertical_lines

---
+++ covidxpert/rotate/detect_almost_vertical_lines.py
@@ -0,0 +1,44 @@
+from itertools import chain
+
+def detect_almost_vertical_lines(
+    image:np.ndarray,
+)->np.ndarray:   
+    """Return image containing almost vertical lines.
+    
+    Parameters
+    ------------------
+    image: np.ndarray,
+        The image to apply the Hough transform to.
+
+    Returns
+    ------------------
+    Returns binary image containing vertical lines.
+    """
+    # Allocate the lines image.
+    median_image = 
+    
+    # Compute almost vertical lines with Hough from given image
+    lines = get_dominant_lines(polar2cartesian(
+        cv2.HoughLines(image, 1, np.pi / 180, 100, None, 0, 0)[:1000]
+    ), *image.shape)
+
+    # Compute almost vertical lines with Hough from given image
+    probabilistic_lines = get_dominant_lines(cv2.HoughLinesP(
+        image,
+        rho=1,
+        theta=np.pi / 180,
+        threshold=100,
+        minLineLength=image.shape[1]//10
+    )[:1000], *image.shape)
+    
+    # Drawing the lines on the image.
+    x0, y0, x1, y1 = np.median(
+        chain(lines, probabilistic_lines),
+        axis=0
+    ).astype(int)
+
+    median_image = cv2.cvtColor(np.zeros_like(image), cv2.COLOR_GRAY2BGR)
+    cv2.line(median_image, (x0, y0), (x1, y1), (1, 1, 1), 5, cv2.LINE_AA)
+
+    # Convert the image back to grayscale
+    return normalize_image(cv2.cvtColor(median_image, cv2.COLOR_BGR2GRAY))
