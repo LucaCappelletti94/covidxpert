de92ca45ecc647bcdfabac250f01ff692fa06661
massimocavallin <massimo.cavallin92@gmail.com>
2020-05-31 00:28:31 +0200

    Added fuzzy test

--- tests/test_get_projected_points.py
+++ tests/test_get_projected_points.py
@@ -6,10 +6,12 @@ import pytest
 
 from covidxpert.utils.test_utils import static_test
 
-def test_get_projected_points():
+
+def test_static_get_projected_points():
     l_tests = [{'Input': (0, 0, 0, 0), 'Output': ZeroDivisionError},
                {'Input': (1, 0, 0, 0), 'Output': (0, 0, 0, 0)},
-               {'Input': (np.inf, np.inf, np.inf, np.inf), 'Output': (np.inf, 0, np.inf, np.inf)},
+               {'Input': (np.inf, np.inf, np.inf, np.inf),
+                'Output': (np.inf, 0, np.inf, np.inf)},
                {'Input': (np.inf, 1, 0, 0), 'Output': (0, 0, 0, 0)},
                {'Input': (-np.inf, 1, 0, 0), 'Output': (0, 0, 0, 0)},
                {'Input': (-1, 2, -3, 4), 'Output': (2.0, 0, -2.0, 4)},
@@ -17,27 +19,20 @@ def test_get_projected_points():
     static_test(get_projected_points,    l_tests)
 
 
-def test_get_projected_points2():
-    key_in: str = 'Input'
-    key_out: str = 'Output'
-    l_tests = [{'Input': (0, 0, 0, 0), 'Output': ZeroDivisionError},
-               {'Input': (1, 0, 0, 0), 'Output': (0, 0, 0, 0)},
-               {'Input': (np.inf, np.inf, np.inf, np.inf), 'Output': (np.inf, 0, np.inf, np.inf)},
-               {'Input': (np.inf, 1, 0, 0), 'Output': (0, 0, 0, 0)},
-               {'Input': (-np.inf, 1, 0, 0), 'Output': (0, 0, 0, 0)},
-               {'Input': (-1, 2, -3, 4), 'Output': (2.0, 0, -2.0, 4)},
-               {'Input': (1, 2, 3, 4), 'Output': (-2.0, 0, 2.0, 4)}]
+def test_fuzzy_get_projected_points2():
+    tests_size = (100, 4)
+    min_val, max_val = -1000, 1000
 
-    def lazy_isin(x): return next((True for d in l_tests if x in d), False)
-    if not lazy_isin(key_in) or not lazy_isin(key_out):
-        raise KeyError(f"{key_in} or {key_out} is not a valid key.")
-
-    for test in l_tests:
-        print(test)
-        if not isinstance(test[key_out], tuple):
-            with pytest.raises(test[key_out]):
-                get_projected_points(*test[key_in])
+    for m, q, x, h in tqdm(
+        np.random.uniform(low=min_val, high=max_val, size=tests_size),
+        desc="Fuzzying test for get_projected_points in range"):
+        if m == 0:
+            with pytest.raises(ZeroDivisionError):
+                x0, y0, x1, y1 = get_projected_points(
+                    m, q, x, int(round(h, 0)))
         else:
-            result = get_projected_points(*test[key_in])
-            assert len(result) == len(test[key_out])
-            assert all(np.isclose(x, y) for x, y in zip(result, test[key_out]))
+            x0, y0, x1, y1 = get_projected_points(m, q, x, h)
+            assert np.isclose(x0, x) if np.isinf(m) else np.isclose(x0, -q / m)
+            assert np.isclose(y0, 0)
+            assert np.isclose(x1, x) if np.isinf(m) else np.isclose(x1, (h - q) / m)
+            assert np.isclose(y1, h)
