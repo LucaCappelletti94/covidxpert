90693b3ddecdb65ed895d5d2942e5a6d8fd7163e
massimocavallin <massimo.cavallin92@gmail.com>
2020-06-11 11:12:06 +0200

    Added darken_msrcr

--- covidxpert/utils/darken.py
+++ covidxpert/utils/darken.py
@@ -2,6 +2,7 @@ from typing import Tuple
 
 import cv2
 import numpy as np
+from covidxpert.utils import automated_msrcr
 
 
 def darken(image: np.ndarray, clip: float = 2.0, kernel: Tuple = (9, 9)) -> np.ndarray:
@@ -24,3 +25,23 @@ def darken(image: np.ndarray, clip: float = 2.0, kernel: Tuple = (9, 9)) -> np.n
     image = clahe.apply(image)
     image = cv2.equalizeHist(image)
     return image
+
+
+def darken_msrcr(image: np.ndarray, sigma_darken: list=[10, 20, 30]) -> np.ndarray:
+    """Return the image with both global and local normalization.
+
+    Parameters
+    --------------------
+    image:np.ndarray,
+        The image to be darkened.
+    sigma_darken:list=[10, 20, 30],
+        Recomemended sigma values [10, 20, 30]
+
+    Returns
+    --------------------
+    Return the darkened image.
+    """
+  darken = automated_msrcr(image, sigma_darken)
+  return darken
+
+
