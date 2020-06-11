6dcc72d2b7afe176b8834ab1a44eb9e9d94855f6
massimocavallin <massimo.cavallin92@gmail.com>
2020-06-11 12:18:22 +0200

    Added auto sigma_darken

--- covidxpert/utils/darken.py
+++ covidxpert/utils/darken.py
@@ -2,32 +2,9 @@ from typing import Tuple
 
 import cv2
 import numpy as np
-from covidxpert.utils import automated_msrcr
+from .retinex import automated_msrcr
 
-
-def darken(image: np.ndarray, clip: float = 2.0, kernel: Tuple = (9, 9)) -> np.ndarray:
-    """Return the image with both global and local normalization.
-
-    Parameters
-    --------------------
-    image:np.ndarray,
-        The image to be darkened.
-    clip:float=15,
-        Maximum local sum after which we clip.
-    kernel:Tuple=(15,15),
-        Kernel size for local histogram normalization.
-
-    Returns
-    --------------------
-    Return the darkened image.
-    """
-    clahe = cv2.createCLAHE(clipLimit=clip, tileGridSize=kernel)
-    image = clahe.apply(image)
-    image = cv2.equalizeHist(image)
-    return image
-
-
-def darken_msrcr(image: np.ndarray, sigma_darken: list=[10, 20, 30]) -> np.ndarray:
+def darken(image: np.ndarray) -> np.ndarray:
     """Return the image with both global and local normalization.
 
     Parameters
@@ -41,6 +18,7 @@ def darken_msrcr(image: np.ndarray, sigma_darken: list=[10, 20, 30]) -> np.ndarr
     --------------------
     Return the darkened image.
     """
+    sigma_darken = range(50, max(image.shape), 100) 
     darken = automated_msrcr(image, sigma_darken)
     return darken
 
