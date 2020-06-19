190840c5e2d58f4fe912b74938f288b6107b9e0b
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2020-06-19 12:24:37 +0200

    Updated perspective correction so to support paired images

--- covidxpert/perspective_correction/perspective_correction.py
+++ covidxpert/perspective_correction/perspective_correction.py
@@ -3,6 +3,7 @@ import cv2
 from ..utils import add_padding
 from .get_corners import get_corners
 from .cut_bounding_box import cut_bounding_box
+from typing import List, Union, Tuple
 
 
 def get_new_cardinals(image: np.ndarray) -> np.ndarray:
@@ -14,16 +15,49 @@ def get_new_cardinals(image: np.ndarray) -> np.ndarray:
     ])
 
 
-def perspective_correction(image: np.ndarray) -> np.ndarray:
+def perspective_correction(image: np.ndarray, others: List[np.ndarray] = None) -> Union[np.ndarray, Tuple[np.ndarray, np.ndarray]]:
+    """Return image with perspective correction.
+
+    Parameters
+    -----------------------
+    image: np.ndarray, 
+        The image whose perspective is to be corrected.
+    others: List[np.ndarray] = None,
+        Optional parameter to specify images to be transformed alongside the
+        given original image.
+
+    Returns
+    ------------------------
+    Either the corrected image or a tuple with both the corrected image and
+    the list of other images associates, if provided.
+    """
     padded = add_padding(image)
     corners, requires_correction, _ = get_corners(padded)
     if not requires_correction:
+        if others is not None:
+            return image, others
         return image
     padded = cut_bounding_box(padded, corners)
     corners -= corners.min(axis=0)
     new_corners = get_new_cardinals(padded)
-    M, _ = cv2.findHomography(
+    M, _ = cv2.findHomography(  # pylint: disable=no-member
         corners,
         new_corners
     )
-    return cv2.warpPerspective(padded, M, (padded.shape[1], padded.shape[0]))
+    result = cv2.warpPerspective(  # pylint: disable=no-member
+        padded,
+        M,
+        (padded.shape[1], padded.shape[0])
+    )
+
+    if others is None:
+        return result
+
+    return result, [
+        cv2.warpPerspective(  # pylint: disable=no-member
+            add_padding(other),
+            M,
+            (padded.shape[1], padded.shape[0])
+        )
+        for other in others
+    ]
