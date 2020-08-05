6e8ef9b1d38e4eea529184eb3499587df53a8392
gbrltv <gabrielmrqstvrs@gmail.com>
2020-08-05 16:50:51 +0200

    Correcting bounding box

--- covidxpert/perspective_correction/cut_bounding_box.py
+++ covidxpert/perspective_correction/cut_bounding_box.py
@@ -2,6 +2,19 @@ import numpy as np
 
 
 def cut_bounding_box(image: np.ndarray, corners: np.ndarray) -> np.ndarray:
-    """Cut the image using the given bounding box."""
-    (min_x, min_y), (max_x, max_y) = corners.min(axis=0), corners.max(axis=0)
+    """Cut the image using the given bounding box.
+    One is added to the maximum (x and y) to include them in the slicing
+
+    Parameters
+    -----------------------
+    image: np.ndarray,
+        The image to be sliced according to corners
+    corners: np.ndarray,
+        Corners of the image
+
+    Returns
+    ------------------------
+    Sliced image
+    """
+    (min_x, min_y), (max_x, max_y) = corners.min(axis=0), corners.max(axis=0) + 1
     return image[min_y:max_y, min_x:max_x]
