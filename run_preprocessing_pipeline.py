a64e4316f5d3b206ada049b0aaeddc9cc36087bb
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2020-08-06 16:24:00 +0200

    Updated run preprocessing pipeline file

--- run_preprocessing_pipeline.py
+++ run_preprocessing_pipeline.py
@@ -4,15 +4,24 @@ from covidxpert import images_pipeline
 from glob import glob
 import pandas as pd
 
-dataset_path = '/data/covidxpert_data/datasets/{}'
-output_path = '/data/covidxpert_data/processed/{}'
-error_path = '/data/covidxpert_data/error_pipeline/'
-width_img = 480
-df_index = pd.read_csv(r'/data/covidxpert_data/datasets/normalized_index/df_all_common_cols.csv')
+root = "~"
+dataset_path = f'{root}/datasets/{{}}'
+output_path = f'{root}/processed/{{}}'
+error_path = f'{root}/error_pipeline/'
+width = 480
+initial_width = 1024
+df_index = pd.read_csv(
+    f'{root}/datasets/normalized_index/df_all_common_cols.csv'
+)
 img_path = [dataset_path.format(path) for path in df_index.img_path]
 out_path = [output_path.format(path) for path in df_index.img_path]
 
 with Notipy():
-    print('start img pipeline')
-    images_pipeline(img_path, out_path, error_path, width=width_img)
-
+    images_pipeline(
+        img_path,
+        out_path,
+        width=width,
+        initial_width=initial_width,
+        cache=True,
+        errors_path=error_path
+    )
