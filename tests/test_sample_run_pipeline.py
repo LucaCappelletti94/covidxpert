7a0c254c98a7d569c00d140dfed463a7a964170c
massimocavallin <massimo.cavallin92@gmail.com>
2020-09-22 18:17:08 +0200

    Added test pipeline for dicom images

---
+++ tests/test_sample_run_pipeline.py
@@ -0,0 +1,37 @@
+import os
+from glob import glob
+from covidxpert import images_pipeline
+import pytest
+
+
+def test_pipeline():
+    """Testing execution of the complete pipeline."""
+    image_paths = glob("tests/sample_run_pipeline/*")
+    output_paths = [
+        "tests/sample_pipeline/{}.jpg".format(
+            "".join(os.path.basename(image_path).split(".")[:-1])
+        )
+        for image_path in image_paths
+    ]
+    images_pipeline(
+        image_paths[:1],
+        output_paths[:1],
+        verbose=False,
+        n_jobs=0
+    )
+    images_pipeline(
+        image_paths,
+        output_paths,
+        save_steps=True,
+        n_jobs=0
+    )
+
+
+def test_pipeline_illegal_arguments():
+    image_paths = glob("tests/sample_pipeline/*")
+    output_paths = []
+    with pytest.raises(ValueError):
+        images_pipeline(
+            image_paths,
+            output_paths,
+        )
