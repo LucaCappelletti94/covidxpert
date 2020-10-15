486bac4ce6e8a907c2ad68e8513f5ea141dc6c4f
zom <tommaso.fontana.96@gmail.com>
2020-10-15 12:24:08 +0200

    exported get_balanced_holdouts

--- covidxpert/__init__.py
+++ covidxpert/__init__.py
@@ -1,5 +1,5 @@
 from .models import resnet, load_keras_model
-from .datasets import load_images
+from .datasets import load_images, get_balanced_holdouts
 from .pipeline import image_pipeline, images_pipeline
 
 __all__ = [
@@ -7,5 +7,6 @@ __all__ = [
     "load_keras_model",
     "load_images",
     "image_pipeline",
-    "images_pipeline"
+    "images_pipeline",
+    "get_balanced_holdouts"
 ]
--- covidxpert/datasets/__init__.py
+++ covidxpert/datasets/__init__.py
@@ -1,4 +1,5 @@
 from .load_images import load_images
+from .get_balanced_holdouts import get_balanced_holdouts
 
 __all__ = [
     "load_images"
