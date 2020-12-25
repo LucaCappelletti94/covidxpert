9ee524cc72d917251c138f26b12704e5fe847555
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2020-12-25 09:23:00 +0100

    Removed notipy since sadly it does not longer work.

--- run_preprocessing_pipeline.py
+++ run_preprocessing_pipeline.py
@@ -1,26 +1,25 @@
 """
     Script that runs the preprocessing pipeline on input images.
 """
-from notipy_me import Notipy
 from covidxpert import images_pipeline
 
 from glob import glob
 import pandas as pd
 import multiprocessing
 
-root = ".."
-dataset_path = f'{root}/datasets/{{}}'
-output_path = f'{root}/processed/{{}}'
-error_path = f'{root}/error_pipeline/'
-width = 480
-initial_width = 1024
-df_index = pd.read_csv(
-    f'{root}/datasets/normalized_index/df_all_common_cols.csv'
-)
-img_path = [dataset_path.format(path) for path in df_index.img_path]
-out_path = [output_path.format(path) for path in df_index.img_path]
+if __name__ == "__main__":
+    root = ".."
+    dataset_path = f'{root}/datasets/{{}}'
+    output_path = f'{root}/processed/{{}}'
+    error_path = f'{root}/error_pipeline/'
+    width = 480
+    initial_width = 1024
+    df_index = pd.read_csv(
+        f'{root}/datasets/normalized_index/df_all_common_cols.csv'
+    )
+    img_path = [dataset_path.format(path) for path in df_index.img_path]
+    out_path = [output_path.format(path) for path in df_index.img_path]
 
-with Notipy():
     images_pipeline(
         img_path,
         out_path,
