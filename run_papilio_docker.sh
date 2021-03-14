a4809aba7ae0a1579f718a81c511d7d0c3546cb4
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2021-03-14 18:48:40 +0100

    Added support for GPU

--- run_papilio_docker.sh
+++ run_papilio_docker.sh
@@ -1,2 +1,4 @@
 docker build --file ROCmDockerfile -t covidxpert_docker .
-docker run --tty --interactive --publish 12000:8888 -v "/data2/covidxpert:/io/data" covidxpert_docker 
+docker run --device=/dev/kfd --device=/dev/dri \
+    --group-add video --security-opt seccomp=unconfined \
+    --tty --interactive --publish 12000:8888 -v "/data2/covidxpert:/io/data" covidxpert_docker
