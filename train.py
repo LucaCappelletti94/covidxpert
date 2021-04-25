bbcd792381f4663caf280122407688190d115ddc
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2021-04-25 22:11:05 +0200

    Added support for mirrored strategy

--- train.py
+++ train.py
@@ -2,6 +2,7 @@
 
 import os
 import silence_tensorflow.auto
+import tensorflow as tf
 import pandas as pd
 from covidxpert import main_train_loop
 
@@ -11,10 +12,13 @@ dataset_name = "processed"
 
 df.img_path = [os.path.join("..", dataset_name, x) for x in df.img_path]
 
-performance = main_train_loop(
-    dataset_name=dataset_name,
-    dataframe=df,
-    img_shape=(256, 256, 1),
-)
+strategy = tf.distribute.MirroredStrategy()
+
+with strategy.scope():
+    performance = main_train_loop(
+        dataset_name=dataset_name,
+        dataframe=df,
+        img_shape=(256, 256, 1),
+    )
 
 performance.to_csv("{}.csv".format(dataset_name), index=False)
