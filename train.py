e7a61d21e9255f7badfd646c5721ebe2f2a3a0b7
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2021-04-29 08:30:23 +0200

    Re-imposted model to resnet50vl

--- train.py
+++ train.py
@@ -3,7 +3,7 @@
 import os
 import silence_tensorflow.auto
 import tensorflow as tf
-from tensorflow.keras.applications import ResNet50V2, InceptionResNetV2
+from tensorflow.keras.applications import ResNet50V2, ResNet50V2
 import pandas as pd
 from covidxpert import main_train_loop
 
@@ -14,9 +14,9 @@ dataset_name = "processed"
 df.img_path = [os.path.join("..", dataset_name, x) for x in df.img_path]
 
 performance = main_train_loop(
-    keras_model=InceptionResNetV2,
-    model_name="InceptionResNetV2",
-    batch_size=128, # We run InceptionNet with batchsize 128 and for the ResNet50V2 we use 256
+    keras_model=ResNet50V2,
+    model_name="ResNet50V2",
+    batch_size=256, # We run InceptionNet with batchsize 128 and for the ResNet50V2 we use 256
     dataset_name=dataset_name,
     dataframe=df,
     img_shape=(480, 480, 1),
