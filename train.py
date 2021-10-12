0eeca3f7bd3220f7e2fdc08a12facbab1c2225d7
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2021-10-12 18:07:28 +0200

    Refactored and cleaned up the train.py file

--- train.py
+++ train.py
@@ -3,23 +3,51 @@
 import os
 import silence_tensorflow.auto
 import tensorflow as tf
-from tensorflow.keras.applications import ResNet50V2, ResNet50V2
+from tensorflow.keras.applications import ResNet50V2, InceptionResNetV2
 import pandas as pd
 from covidxpert import main_train_loop
+from tqdm.auto import tqdm
 
-df = pd.read_csv("../df_all_common_cols.csv")
+dataset_names = [
+    "processed",
+    "demosaicking_processed",
+    "resized_processed",
+]
 
-dataset_name = "resized_processed"
+models = {
+    "ResNet50V2": dict(
+        keras_model=ResNet50V2,
+        batch_size=256
+    ),
+    "InceptionResNetV2": dict(
+        keras_model=InceptionResNetV2,
+        batch_size=128
+    )
+}
 
-df.img_path = [os.path.join("..", dataset_name, x) for x in df.img_path]
+for dataset_name in tqdm(
+    dataset_names,
+    desc="Elaborating datasets"
+):
+    df = pd.read_csv("../df_all_common_cols.csv")
+    df.img_path = [os.path.join("..", dataset_name, x) for x in df.img_path]
+    for model_name, model_parameters in tqdm(
+        models.items(),
+        total=len(models),
+        desc="Training models on dataset {}".format(dataset_name)
+    ):
+        performance = main_train_loop(
+            **model_parameters,
+            model_name=model_name,
+            dataset_name=dataset_name,
+            dataframe=df,
+            img_shape=(480, 480, 1),
+        )
 
-performance = main_train_loop(
-    keras_model=ResNet50V2,
-    model_name="ResNet50V2",
-    batch_size=256, # We run InceptionNet with batchsize 128 and for the ResNet50V2 we use 256
-    dataset_name=dataset_name,
-    dataframe=df,
-    img_shape=(480, 480, 1),
-)
-
-performance.to_csv("{}.csv".format(dataset_name), index=False)
+        performance.to_csv(
+            "{}_{}.csv".format(
+                model_name,
+                dataset_name
+            ),
+            index=False
+        )
