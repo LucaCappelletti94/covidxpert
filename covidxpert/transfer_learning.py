cdcb90cae2238be947f5c8f09088643e4cd1e968
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2021-11-16 10:01:48 +0100

    Added missing metadat

--- covidxpert/transfer_learning.py
+++ covidxpert/transfer_learning.py
@@ -439,6 +439,7 @@ def run_holdout(
                 verbose=verbose,
                 cache_dir=cache_dir,
             )
+            perf["holdout_number"] = holdout_number
             perf["task_name"] = task_name
             total_perf.append(perf)
     return pd.concat(total_perf)
